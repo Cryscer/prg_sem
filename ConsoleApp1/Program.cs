66bcc4205a3192528a484090dd1262795ad6299f
Dao-Xuan Viet <dao-xuan@gymvod.local>
2024-09-13 09:34:03 +0200

    added my first console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 5f;
+            //int / intem je vždy jen int, jedna z proměn musí být jiná, píše se f konci u floatu
+            int b = 4;
+            float result = a / b;
+            Console.WriteLine(result);
+
+            Console.WriteLine("Hello\nWorld");
+            Console.ReadKey();
+        }
+    }
+}
