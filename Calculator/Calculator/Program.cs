34d1e766ec3868cbc76f2f922880b38b66d28be3
Cryscer <Vietdaocz@gmail.com>
2024-10-08 16:42:39 +0200

    Update Program.cs

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -20,36 +20,36 @@ namespace Calculator
                     double result = 0; // result musim mit na double aby mi fungovaly odmocniny
                     Console.WriteLine("zadejte prvni cislo");
                     string firstNum = Console.ReadLine();
-                    float a = float.Parse(firstNum);
+                    float inputA = float.Parse(firstNum);
                     Console.WriteLine("Chcete zadat dalsi cislo? (zadejte ano nebo ne)");
-                    string odpovedA = Console.ReadLine();
-                    if (odpovedA == "ano")
+                    string answerA = Console.ReadLine();
+                    if (answerA == "ano")
                     {
                         Console.WriteLine("zadejte druhe cislo");
                         string scndNum = Console.ReadLine();
-                        float b = float.Parse(scndNum);
+                        float inputB = float.Parse(scndNum);
                         Console.WriteLine("zadejte co s nima (moznosti jsou soucet, rozdil, nasobeni a deleni)"); //pridal jsem nasobeni a deleni
-                        string operaceA = Console.ReadLine();
+                        string operationA = Console.ReadLine();
                         
                         x = 0;
-                        if (operaceA == "soucet")
+                        if (operationA == "soucet")
                         {
-                            result = a + b;
+                            result = inputA + inputB;
                             Console.WriteLine("vysledek je " + result);
                         }
-                        else if (operaceA == "rozdil")
+                        else if (operationA == "rozdil")
                         {
-                            result = a - b;
+                            result = inputA - inputB;
                             Console.WriteLine("vysledek je " + result);
                         }
-                        else if (operaceA == "nasobeni")
+                        else if (operationA == "nasobeni")
                         {
-                            result = a * b;
+                            result = inputA * inputB;
                             Console.WriteLine("vysledek je " + result);
                         }
-                        else if (operaceA == "deleni")
+                        else if (operationA == "deleni")
                         {
-                            result = a / b;
+                            result = inputA / inputB;
                             Console.WriteLine("vysledek je " + result);
                         }
                         else
@@ -58,54 +58,54 @@ namespace Calculator
                             x = 1;
                         }
                     }
-                    else if (odpovedA == "ne")
+                    else if (answerA == "ne")
                     { 
                         x = 0;
                         Console.WriteLine("zadejte co s tim cislem (moznosti jsou mocneni, odmocnovani, faktorial a prevod, coz znamena prevod na dvojkovou soustavu)"); //krome mocneni a odmocnovani jsem taky pridal operaci faktorial
-                        string operaceB = Console.ReadLine();
-                        if (operaceB == "mocneni")
+                        string operationB = Console.ReadLine();
+                        if (operationB == "mocneni")
                         {
                             Console.WriteLine("na kolikatou chcete mocnit?");
-                            string mocnina = Console.ReadLine();
-                            float mocninaCislo = float.Parse(mocnina);
+                            string power = Console.ReadLine();
+                            float powerExponent = float.Parse(power);
                             result = 1;
-                            for (int i = 0; i < mocninaCislo; i++)
+                            for (int i = 0; i < powerExponent; i++)
                             {
-                                result = result * a;
+                                result = result * inputA;
                             }
                             Console.WriteLine("vysledek je " + result);
                         }
-                        else if (operaceB == "odmocnovani")
+                        else if (operationB == "odmocnovani")
                         {
                             Console.WriteLine("na kolikatou chcete odmocnovat?");
-                            string odmocnina = Console.ReadLine();
-                            float odmocninaCislo = float.Parse(odmocnina);
-                            result = Math.Pow(a, (1 / odmocninaCislo)); //tohle jsem si nasel na internetu ve stack overflow
+                            string root = Console.ReadLine();
+                            float rootExponent = float.Parse(root);
+                            result = Math.Pow(inputA, (1 / rootExponent)); //tohle jsem si nasel na internetu ve stack overflow
                             Console.WriteLine("vysledek je " + result);
                         }
-                        else if (operaceB == "faktorial")
+                        else if (operationB == "faktorial")
                         {
                             result = 1;
-                            for (int i = 1; i < a+1; i++)
+                            for (int i = 1; i < inputA+1; i++)
                             {
                                 result = result * i;
                             }                            
                             Console.WriteLine("vysledek je " + result);
                         }
-                        else if (operaceB == "prevod") //tady jsem pridal prevod na dvojkovou soustavu
+                        else if (operationB == "prevod") //tady jsem pridal prevod na dvojkovou soustavu
                         {
-                            while (a>1)
+                            while (inputA>1)
                             {
                                 float y = 1;
                                 float i = 0;
-                                for (i = 0; a > Math.Pow(2, (i + 1)); i++)
+                                for (i = 0; inputA > Math.Pow(2, (i + 1)); i++)
                                 {
                                     y = y * 2;
                                 }
                                 result = result + Math.Pow(10,(i));
-                                a = a - y;
+                                inputA = inputA - y;
                             }
-                            result = result + a;
+                            result = result + inputA;
                             Console.WriteLine("vysledek je " + result);
                         }
                         else
