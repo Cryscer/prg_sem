b383c9dd8856796c6c788ef58e88858d6f30604b
Cryscer <Vietdaocz@gmail.com>
2024-11-27 19:15:49 +0100

    started battleships

---
+++ Battleships/Battleships/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    internal class Program
+    {
+        static void PrintArray(string[,] arrayToPrint)
+        {
+            for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayToPrint.GetLength(1); j++) Console.Write(arrayToPrint[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[,] playerField = new string[10,10];
+            for (int i = 0; i < playerField.GetLength(0); i++) for (int j = 0; j < playerField.GetLength(1); j++) playerField[i,j] = "O";
+            
+            PrintArray(playerField);
+            Console.ReadLine();
+        }
+    }
+}
