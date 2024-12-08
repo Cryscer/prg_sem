9f26079e75784ff51d105594421ce09584fc7d58
Cryscer <Vietdaocz@gmail.com>
2024-12-08 19:09:05 +0100

    started working on pc placement

--- Battleships/Battleships/Program.cs
+++ Battleships/Battleships/Program.cs
@@ -8,38 +8,23 @@ namespace Battleships
 {
     internal class Program
     {
-        static void PrintArray(string[,] arrayPrintPlayer, string[,] arrayPrintPC)
+        static void PrintArray(string[,] arrayToPrint)
         {
-            Console.WriteLine("      Vaše pole");
-            Console.WriteLine("   a b c d e f g h i j");            
-            for (int i = 0; i < arrayPrintPlayer.GetLength(0); i++)
+            Console.WriteLine("   a b c d e f g h i j");
+            for (int i = 0; i < arrayToPrint.GetLength(0); i++)
             {
                 if (i == 9) Console.Write(i + 1 + " "); else Console.Write(i + 1 + "  ");
-                for (int j = 0; j < arrayPrintPlayer.GetLength(1); j++) Console.Write(arrayPrintPlayer[i, j] + " ");
+                for (int j = 0; j < arrayToPrint.GetLength(1); j++) Console.Write(arrayToPrint[i, j] + " ");
                 Console.WriteLine();
             }
             Console.WriteLine();
-            Console.WriteLine("    Pole nepřítele");
-            Console.WriteLine("   a b c d e f g h i j");
-            for (int i = 0; i < arrayPrintPC.GetLength(0); i++)
-            {
-                if (i == 9) Console.Write(i + 1 + " "); else Console.Write(i + 1 + "  ");
-                for (int j = 0; j < arrayPrintPC.GetLength(1); j++) Console.Write(arrayPrintPC[i, j] + " ");
-                Console.WriteLine();
-            }
         }
-        static void Main(string[] args)
+        static void PlayerPlacement(string[,] playerPlacementArray)
         {
-            string[,] playerField = new string[10, 10];
-            for (int i = 0; i < playerField.GetLength(0); i++) for (int j = 0; j < playerField.GetLength(1); j++) playerField[i, j] = "O";
-            string[,] computerField = (string[,])playerField.Clone();
-            PrintArray(playerField, computerField);
-            
-            List<string> columns = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-            List<string> shipTypesLetters = new List<string> { "T", "P", "K", "B", "L"};
-            List<string> shipTypes = new List<string> { "Torpédoborec", "Ponorka", "Křižník", "Bitevní loď", "Letadlová loď" };                                               
+            List<string> columns = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+            List<string> shipTypesLetters = new List<string> { "T", "P", "K", "B", "L" };
+            List<string> shipTypes = new List<string> { "Torpédoborec", "Ponorka", "Křižník", "Bitevní loď", "Letadlová loď" };
             bool repeat;
-
             for (int i = 0; i < shipTypes.Count; i++)
             {
                 do
@@ -61,7 +46,7 @@ namespace Battleships
                                 {
                                     for (int k = 0; k < shipIndex + 1; k++)
                                     {
-                                        if (shipTypesLetters.Contains(playerField[xCoordinate - 1 - k, yCoordinate]))
+                                        if (shipTypesLetters.Contains(playerPlacementArray[xCoordinate - 1 - k, yCoordinate]))
                                         {
                                             Console.WriteLine("Lodě nemohou ležet přes sebe!");
                                             repeat = true;
@@ -69,7 +54,7 @@ namespace Battleships
                                     }
                                     if (repeat == false)
                                     {
-                                        for (int j = 0; j < shipIndex + 1; j++) playerField[xCoordinate - 1 - j, yCoordinate] = shipTypesLetters[shipIndex];
+                                        for (int j = 0; j < shipIndex + 1; j++) playerPlacementArray[xCoordinate - 1 - j, yCoordinate] = shipTypesLetters[shipIndex];
                                     }
                                     break;
                                 }
@@ -77,7 +62,7 @@ namespace Battleships
                                 {
                                     for (int k = 0; k < shipIndex + 1; k++)
                                     {
-                                        if (shipTypesLetters.Contains(playerField[xCoordinate, yCoordinate + 1 + k]))
+                                        if (shipTypesLetters.Contains(playerPlacementArray[xCoordinate, yCoordinate + 1 + k]))
                                         {
                                             Console.WriteLine("Lodě nemohou ležet přes sebe!");
                                             repeat = true;
@@ -85,7 +70,7 @@ namespace Battleships
                                     }
                                     if (repeat == false)
                                     {
-                                        for (int j = 0; j < shipIndex + 1; j++) playerField[xCoordinate, yCoordinate + 1 + j] = shipTypesLetters[shipIndex];
+                                        for (int j = 0; j < shipIndex + 1; j++) playerPlacementArray[xCoordinate, yCoordinate + 1 + j] = shipTypesLetters[shipIndex];
                                     }
                                     break;
                                 }
@@ -93,7 +78,7 @@ namespace Battleships
                                 {
                                     for (int k = 0; k < shipIndex + 1; k++)
                                     {
-                                        if (shipTypesLetters.Contains(playerField[xCoordinate + 1 + k, yCoordinate]))
+                                        if (shipTypesLetters.Contains(playerPlacementArray[xCoordinate + 1 + k, yCoordinate]))
                                         {
                                             Console.WriteLine("Lodě nemohou ležet přes sebe!");
                                             repeat = true;
@@ -101,7 +86,7 @@ namespace Battleships
                                     }
                                     if (repeat == false)
                                     {
-                                        for (int j = 0; j < shipIndex + 1; j++) playerField[xCoordinate + 1 + j, yCoordinate] = shipTypesLetters[shipIndex];
+                                        for (int j = 0; j < shipIndex + 1; j++) playerPlacementArray[xCoordinate + 1 + j, yCoordinate] = shipTypesLetters[shipIndex];
                                     }
                                     break;
                                 }
@@ -109,7 +94,7 @@ namespace Battleships
                                 {
                                     for (int k = 0; k < shipIndex + 1; k++)
                                     {
-                                        if (shipTypesLetters.Contains(playerField[xCoordinate, yCoordinate - 1 - k]))
+                                        if (shipTypesLetters.Contains(playerPlacementArray[xCoordinate, yCoordinate - 1 - k]))
                                         {
                                             Console.WriteLine("Lodě nemohou ležet přes sebe!");
                                             repeat = true;
@@ -117,7 +102,7 @@ namespace Battleships
                                     }
                                     if (repeat == false)
                                     {
-                                        for (int j = 0; j < shipIndex + 1; j++) playerField[xCoordinate, yCoordinate - 1 - j] = shipTypesLetters[shipIndex];
+                                        for (int j = 0; j < shipIndex + 1; j++) playerPlacementArray[xCoordinate, yCoordinate - 1 - j] = shipTypesLetters[shipIndex];
                                     }
                                     break;
                                 }
@@ -128,12 +113,12 @@ namespace Battleships
                                     break;
                                 };
                         }
-                        if (shipTypesLetters.Contains(playerField[xCoordinate, yCoordinate]))
+                        if (shipTypesLetters.Contains(playerPlacementArray[xCoordinate, yCoordinate]))
                         {
                             Console.WriteLine("Lodě nemohou ležet přes sebe!");
                             repeat = true;
                         }
-                        else if (repeat == false) playerField[xCoordinate, yCoordinate] = shipTypesLetters[shipIndex];                                                
+                        else if (repeat == false) playerPlacementArray[xCoordinate, yCoordinate] = shipTypesLetters[shipIndex];
                     }
                     catch (Exception)
                     {
@@ -141,8 +126,81 @@ namespace Battleships
                         repeat = true;
                     }
                 } while (repeat);
-                PrintArray(playerField, computerField);
-            }                                           
+                Console.WriteLine("      Vaše pole");
+                PrintArray(playerPlacementArray);
+            }
+        }
+        static void ComputerPlacement(string[,] computerPlacementArray)
+        {
+            List<string> shipTypesLetters = new List<string> { "T", "P", "K", "B", "L" };
+            bool repeat;
+            Random rng = new Random();
+            for (int i = 0; i < shipTypesLetters.Count; i++)
+            {
+                do
+                {
+                    repeat = false;
+                    try
+                    {
+                        Console.WriteLine(shipTypesLetters[i]);
+                        int shipIndex = i;
+                        int xCoordinate = rng.Next(0, 10);
+                        int yCoordinate = rng.Next(0, 10);
+                        int direction = rng.Next(1, 5);
+                        switch (direction)
+                        {
+                            case 1:
+                                {
+                                    for (int k = 0; k < shipIndex + 1; k++) if (shipTypesLetters.Contains(computerPlacementArray[xCoordinate - 1 - k, yCoordinate])) repeat = true;
+                                    if (repeat == false) for (int j = 0; j < shipIndex + 1; j++) computerPlacementArray[xCoordinate - 1 - j, yCoordinate] = shipTypesLetters[shipIndex];
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    for (int k = 0; k < shipIndex + 1; k++) if (shipTypesLetters.Contains(computerPlacementArray[xCoordinate, yCoordinate + 1 + k])) repeat = true;
+                                    if (repeat == false) for (int j = 0; j < shipIndex + 1; j++) computerPlacementArray[xCoordinate, yCoordinate + 1 + j] = shipTypesLetters[shipIndex];
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    for (int k = 0; k < shipIndex + 1; k++) if (shipTypesLetters.Contains(computerPlacementArray[xCoordinate + 1 + k, yCoordinate])) repeat = true;
+                                    if (repeat == false) for (int j = 0; j < shipIndex + 1; j++) computerPlacementArray[xCoordinate + 1 + j, yCoordinate] = shipTypesLetters[shipIndex];
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    for (int k = 0; k < shipIndex + 1; k++) if (shipTypesLetters.Contains(computerPlacementArray[xCoordinate, yCoordinate - 1 - k])) repeat = true;
+                                    if (repeat == false) for (int j = 0; j < shipIndex + 1; j++) computerPlacementArray[xCoordinate, yCoordinate - 1 - j] = shipTypesLetters[shipIndex];
+                                    break;
+                                }
+                            default:
+                                {
+                                    repeat = true;
+                                    break;
+                                };
+                        }
+                        if (shipTypesLetters.Contains(computerPlacementArray[xCoordinate, yCoordinate])) repeat = true;
+                        else if (repeat == false) computerPlacementArray[xCoordinate, yCoordinate] = shipTypesLetters[shipIndex];
+                    }
+                    catch (Exception)
+                    {
+                        repeat = true;
+                    }   
+                } while (repeat);               
+            }
+            Console.WriteLine("   Pole nepřítele");
+            PrintArray(computerPlacementArray);
+        }
+        static void Main(string[] args)
+        {
+            string[,] playerField = new string[10, 10];
+            for (int i = 0; i < playerField.GetLength(0); i++) for (int j = 0; j < playerField.GetLength(1); j++) playerField[i, j] = "O";
+            string[,] computerField = (string[,])playerField.Clone();
+            Console.WriteLine("      Vaše pole");
+            PrintArray(playerField);
+            ComputerPlacement(computerField);
+            
+                                                       
             Console.ReadLine();
         }
     }
