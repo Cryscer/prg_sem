69098b93bc9a18ad955afe7f2d491870b36608a2
Dao-Xuan Viet <dao-xuan@gymvod.local>
2025-01-10 09:36:40 +0100

    started classes

---
+++ Classes/Classes/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Classes
+{
+    internal class Enemy
+    {
+        int health;
+        int healthBase;
+        int damage;
+        int damageBase;
+        int level;
+        private Random rng;
+
+        public Enemy (int healthBase, int damageBase, int level)
+        {
+            this.healthBase = healthBase;
+            health = this.healthBase * level;
+            this.damageBase = damageBase;
+            damage = this.damageBase * level;
+            this.level = level;
+            rng = new Random();
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public int GetDamage()
+        {
+            return damage;
+        }
+
+        public int GetRandomizedDamage()
+        {
+            return rng.Next(damage / 2, damage + 1);
+        }
+
+        public void Hurt(int value)
+        {
+            health -= value;
+            Console.WriteLine("enemy got hit for " + value + " damage");
+            if (health <= 0) Console.WriteLine("Enemy is dead");
+        }
+
+        public bool IsDead()
+        {
+            return health <= 0;
+        }
+    }
+}
---
+++ Classes/Classes/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Player
+    {
+        int health;
+        public int damage;
+        public string name;
+        private Random rng;
+
+        public Player(int health, int damage, string name)
+        {
+            SetHealth(health);
+            this.damage = damage;
+            this.name = name;
+            rng = new Random();
+        }
+
+        public void SetHealth(int value)
+        {
+            health = value;
+            if (health < 0) health = 0;
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public int GetDamage()
+        {
+            return damage;
+        }
+
+        public int GetRandomizedDamage()
+        {
+            return rng.Next(damage / 2, damage + damage / 2 + 1);
+        }
+
+        public void Hurt(int value)
+        {
+            health -= value;
+            Console.WriteLine("player got hit for " + value + " damage");
+            if (health <= 0) Console.WriteLine("Player is dead");
+        }
+
+        public bool IsDead()
+        {
+            return health <= 0;
+        }
+    }
+}
