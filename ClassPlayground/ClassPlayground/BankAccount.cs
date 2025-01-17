e5ce46992d03b373c9872df58c39d6722e2e486d
Dao-Xuan Viet <dao-xuan@gymvod.local>
2025-01-17 09:31:12 +0100

    completed Rectangle and nearly finished BankAccount

---
+++ ClassPlayground/ClassPlayground/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class BankAccount
+    {
+        //note static funkce jsou k pouziti funkce bez vytvoreni specificke nove instance (BankAccount account1 = new account), tedy nemusime si vytvorit account1. Dokonce i writeline je staticka funkce
+        int accountNumber;
+        string holderName;
+        string currency;
+        int balance;
+
+        public BankAccount(int accountNumber, string holderName, string currency, int balance)
+        {
+            this.accountNumber = accountNumber;
+            this.holderName = holderName;
+            this.currency = currency;
+            this.balance = balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            if (amount < 0) amount = 0;
+            balance += amount;
+        }
+
+        public int Withdraw(int amount)
+        {
+            if (amount < 0) amount = 0;
+            if (amount > balance)
+            {
+                amount = 0;
+                Console.WriteLine("Not enough money in account!");
+            }
+            balance -= amount;
+            return amount;
+        }
+        static void Transfer(BankAccount accountFrom, BankAccount accountTo, int amount) 
+        {
+            if (amount < 0)
+            {
+                amount = 0;
+                Console.WriteLine("Cannot transfer a ngative amount!");
+            }
+            else if (amount > accountFrom.balance)
+            {
+                amount = 0;
+                Console.WriteLine("Not enough money in account!");
+            }
+            else Console.WriteLine("Successfully transfered");
+            accountFrom.balance -= amount;
+            accountTo.balance += amount;           
+        }
+    }
+}
---
+++ ClassPlayground/ClassPlayground/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        public float width, height;
+
+        public Rectangle(float width, float height) 
+        {
+            if (width < 0) width = -1 * width;
+            if (height < 0) height = -1 * height;  
+            this.height = height;
+            this.width = width;
+        }
+
+        public void CalculateArea()
+        {
+            float value = width * height;
+            Console.Write("the total area is " + value);
+            
+        }
+
+        public void CalculateAspectRatio()
+        {
+            float aspectRatio = (width / height);
+            if (aspectRatio > 1) Console.WriteLine(" wide");
+            else if (aspectRatio < 1) Console.WriteLine(" tall");
+            else Console.WriteLine(" and it is a square");           
+        }
+
+        public void ContainsPoint(float x, float y)
+        {
+            if (x < width || y < height) Console.WriteLine("yes, this point is located inside the rectangle");
+            else Console.WriteLine("no, this point isn't located inside the rectangle");
+        } 
+    }
+}
