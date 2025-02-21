f7399e867583ac99a74fb1b473411d559c1ab0bb
Cryscer <Vietdaocz@gmail.com>
2025-02-21 16:50:26 +0100

    first steps

---
+++ game/game/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    internal class Character
+    {
+        int vitality, strength, endurance, dexterity, attackBonus, damageBonus, evasion, hp, damageDice;
+
+        public Character(int vitality, int strength, int endurance, int dexterity)
+        {
+            attackBonus = strength + dexterity;
+            damageBonus = strength;
+            evasion = 10 + dexterity;
+            hp = 5 * vitality;
+            damageDice = 6;
+        }
+
+        static void Attack(Character attacker, Character defender)
+        {
+            Random rng = new Random();
+            int chance = rng.Next(1, 21) + attacker.attackBonus;
+            if (chance < defender.evasion) Console.WriteLine("Miss");
+            else
+            {
+                int damage = rng.Next(1, attacker.damageDice + 1) + attacker.damageBonus;
+                defender.hp -= damage;
+                Console.WriteLine("Attack dealt " + damage + " damage");
+            }
+
+        }
+
+    }
+}
---
+++ game/game/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    internal class Program
+    {        
+        
+      
+        static void Main(string[] args)
+        {                       
+            Console.WriteLine("this is the player ");
+            
+            int vitality = 1;
+            int endurance = 1;
+            int strength = 1;
+            int dexterity = 1;
+            
+            int attackBonus = strength + dexterity;
+            int damageBonus = strength;
+            int evasiveness = 10 + dexterity;
+            int hp = 10 + vitality;
+
+        }
+    }
+}
