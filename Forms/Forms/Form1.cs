5ed8b622ab91eb4e980ca4090a3cfc7cda5549a0
Dao-Xuan Viet <dao-xuan@gymvod.local>
2025-03-14 09:23:15 +0100

    forms

---
+++ Forms/Forms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Cancel")
+            {
+                textBox1.Text = "";
+                button1.Text = "Button";
+            }
+            else
+            {
+                textBox1.Text = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+                button1.Text = "Cancel";
+            }
+        }
+    }
+}
