995b614f128b1a289971fcf1923698352de70054
Dao-Xuan Viet <dao-xuan@gymvod.local>
2025-03-28 09:40:40 +0100

    paint more

--- paint/paint/Form1.Designer.cs
+++ paint/paint/Form1.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.trackBarPen = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPen = new System.Windows.Forms.Button();
+            this.buttonEllipse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPen)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -42,21 +49,92 @@
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
             this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(447, 12);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(74, 32);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear panel";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(447, 50);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(74, 33);
+            this.buttonColor.TabIndex = 2;
+            this.buttonColor.Text = "Color";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // trackBarPen
+            // 
+            this.trackBarPen.Location = new System.Drawing.Point(536, 38);
+            this.trackBarPen.Name = "trackBarPen";
+            this.trackBarPen.Size = new System.Drawing.Size(122, 45);
+            this.trackBarPen.TabIndex = 3;
+            this.trackBarPen.Scroll += new System.EventHandler(this.trackBarPen_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(574, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pen size";
+            // 
+            // buttonPen
+            // 
+            this.buttonPen.Location = new System.Drawing.Point(448, 139);
+            this.buttonPen.Name = "buttonPen";
+            this.buttonPen.Size = new System.Drawing.Size(73, 34);
+            this.buttonPen.TabIndex = 5;
+            this.buttonPen.Text = "Pen";
+            this.buttonPen.UseVisualStyleBackColor = true;
+            this.buttonPen.Click += new System.EventHandler(this.buttonElipsa_Click);
+            // 
+            // buttonEllipse
+            // 
+            this.buttonEllipse.Location = new System.Drawing.Point(527, 139);
+            this.buttonEllipse.Name = "buttonEllipse";
+            this.buttonEllipse.Size = new System.Drawing.Size(73, 34);
+            this.buttonEllipse.TabIndex = 6;
+            this.buttonEllipse.Text = "O";
+            this.buttonEllipse.UseVisualStyleBackColor = true;
+            this.buttonEllipse.Click += new System.EventHandler(this.buttonEllipse_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(893, 574);
+            this.Controls.Add(this.buttonEllipse);
+            this.Controls.Add(this.buttonPen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarPen);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPen)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.TrackBar trackBarPen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPen;
+        private System.Windows.Forms.Button buttonEllipse;
     }
 }
 
--- paint/paint/Form1.cs
+++ paint/paint/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,18 +16,32 @@ namespace paint
         System.Drawing.Point lastPosition;
         Graphics g;
         bool mousePressed;
+        Pen mainPen = new Pen(Color.Black, 1);
+        string tool = "pen";
+        GraphicsState newState;
         public Form1()
         {
             InitializeComponent();
             g = panel1.CreateGraphics();
+            newState = g.Save();
         }
 
         private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
             if (mousePressed)
             {
-                g.DrawLine(Pens.Black, e.Location, lastPosition);
-                lastPosition = e.Location;
+                switch (tool)
+                {
+                    case "pen":
+                        g.DrawLine(mainPen, e.Location, lastPosition);
+                        lastPosition = e.Location;
+                        newState = g.Save();
+                        break;
+                    case "ellipse":
+                        g.Restore(newState);
+                        g.DrawEllipse(mainPen, lastPosition.X, lastPosition.Y, e.X - lastPosition.X, e.Y - lastPosition.Y);
+                        break;
+                }
             }
         }
 
@@ -39,6 +54,35 @@ namespace paint
         private void panel1_MouseUp(object sender, MouseEventArgs e)
         {
             mousePressed = false;
+            newState = g.Save();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog myColorDialog = new ColorDialog();
+            myColorDialog.Color = mainPen.Color;
+            myColorDialog.ShowDialog();
+            mainPen.Color = myColorDialog.Color;
+        }
+
+        private void trackBarPen_Scroll(object sender, EventArgs e)
+        {
+            mainPen.Width = trackBarPen.Value;
+        }
+
+        private void buttonElipsa_Click(object sender, EventArgs e)
+        {
+            tool = "pen";
+        }
+
+        private void buttonEllipse_Click(object sender, EventArgs e)
+        {
+            tool = "ellipse";
         }
     }
 }
